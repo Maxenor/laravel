e36adcd1b2ada3482a5eb9a06515fd9a3a9399a9
Maxime Michon <71068056+Maxenor@users.noreply.github.com>
2022-01-12 21:39:42 +0100

    ep 1-8

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/app.css">
+    <title>My blog</title>
+</head>
+<body>
+    <article>
+        <?php echo $post; ?>
+    </article>
+    <a href="/">Go back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/app.css">
+    <title>My blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut massa vel diam sagittis bibendum in et enim. Nam non nisi justo. Maecenas fringilla quam eu arcu sollicitudin, vel pharetra neque tempus. Morbi orci ex, efficitur sit amet rhoncus quis, pharetra rutrum erat. Nam tincidunt tristique nulla ut ultrices.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut massa vel diam sagittis bibendum in et enim. Nam non nisi justo. Maecenas fringilla quam eu arcu sollicitudin, vel pharetra neque tempus. Morbi orci ex, efficitur sit amet rhoncus quis, pharetra rutrum erat. Nam tincidunt tristique nulla ut ultrices.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut massa vel diam sagittis bibendum in et enim. Nam non nisi justo. Maecenas fringilla quam eu arcu sollicitudin, vel pharetra neque tempus. Morbi orci ex, efficitur sit amet rhoncus quis, pharetra rutrum erat. Nam tincidunt tristique nulla ut ultrices.</p>
+    </article>
+
+</body>
+</html>
